c6b188a244d7249b4703bbdd7875bbcce7766ee5
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2024-01-19 15:58:44 +0100

    first commit

---
+++ blockchain.class.js
@@ -0,0 +1,19 @@
+class Blockchain {
+    constructor(){
+        this.chain = [];
+    }
+
+    addBlock(block) {
+        // Todo
+    }
+
+
+    isValid() {
+        // Todo
+    }
+
+
+    getLastBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+}
