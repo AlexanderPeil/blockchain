9a23cb557ef2f7d7fc40295cd228ab59d276c98a
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2024-01-21 23:15:25 +0100

    added addBlock function and a isValid check

--- blockchain.class.js
+++ blockchain.class.js
@@ -1,15 +1,24 @@
 class Blockchain {
-    constructor(){
+    constructor() {
         this.chain = [];
     }
 
     addBlock(block) {
-        // Todo
+        block.lastHash = this.getLastBlock().createHash();
+        this.chain.push(Object.freeze(block));
     }
 
 
     isValid() {
-        // Todo
+        let invalidBlock = this.chain.find((currBlock, i) => {
+            let prevBlock = this.chain[i - 1];
+            return prevBlock && prevBlock.createHash() != currBlock.lastHash;
+        });
+        if (invalidBlock) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
 
